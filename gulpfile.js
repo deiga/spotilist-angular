4f2ab8df1ebf4f1fed9cc4dac784a838002394cf
Timo Sand <timo.sand@eficode.com>
2014-10-12 14:08:03 +0300

    Added task listing

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var gulp = require('gulp');
+var taskListing = require('gulp-task-listing');
 
 require('require-dir')('./gulp');
 
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
+
+gulp.task('help', taskListing);
